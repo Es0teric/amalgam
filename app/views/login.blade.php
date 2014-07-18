9d0e1f67265e19c34c88e3baf60948ceb5b4796e
Gamaliel Fulgencio <leo.fulgencio@gmail.com>
2014-07-18 17:16:16 -0400

    added login view, compiled sass and added debugbar package, removed .sass-cache from .gitignore

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -5,52 +5,64 @@
 	</head>
 	
 	<body>
-
+	<!--
 		<div class="cards">
-  <div class="card">
-    <div class="card-image">
-      <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/mountains.png" alt="">
-    </div>
-    <div class="card-header">
-      First Card
-    </div>
-    <div class="card-copy">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, officiis sunt neque facilis culpa molestiae necessitatibus delectus veniam provident.</p>
-    </div>
-    <div class="card-stats">
-      <ul>
-        <li>98<span>Items</span></li>
-        <li>298<span>Things</span></li>
-        <li>923<span>Objects</span></li>
-      </ul>
-    </div>
-  </div>
-  </div>
+		  <div class="card">
+		    <div class="card-image">
+		      <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/mountains.png" alt="">
+		    </div>
+		    <div class="card-header">
+		      First Card
+		    </div>
+		    <div class="card-copy">
+		      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, officiis sunt neque facilis culpa molestiae necessitatibus delectus veniam provident.</p>
+		    </div>
+		    <div class="card-stats">
+		      <ul>
+		        <li>98<span>Items</span></li>
+		        <li>298<span>Things</span></li>
+		        <li>923<span>Objects</span></li>
+		      </ul>
+		    </div>
+		  </div>
+		  </div>
+	-->
 
 
-		<div class="container">
-		
-			<h2>Login</h2>
+		<div id="container">
 
-			{{ Form::open(array('url' => 'login', 'method' => 'POST')) }}
+			<div class="login_container"> 
 
-				<!-- are there errors? -->
-				<p>
-					{{ $errors->first('email') }}
-					{{ $errors->first('pass') }}
-				</p>
+				<h2>Login</h2>
 
-				{{ Form::label('email', 'Email:') }}
-				{{ Form::text('email') }}
-				<br>
-				{{ Form::label('password', "Password:") }}
-				{{ Form::password('pass') }}
-				<br>
-				{{ Form::checkbox('remember_user') . ' Remember Me' }} 
-				<br>
-				{{ Form::submit('Login!') }}
+				{{ Form::open(array('url' => 'login', 'method' => 'POST')) }}
 
-			{{ Form::close() }}
+					<!-- are there errors? -->
+					<p>
+						{{ $errors->first('email') }}
+						{{ $errors->first('pass') }}
+					</p>
+					<div class="email">
+						{{ Form::label('email', 'Email:') }}
+						{{ Form::text('email') }}
+					</div>
+					
+					<div class="pass">
+						{{ Form::label('password', "Password:") }}
+						{{ Form::password('pass', array('id' => 'pass')) }}
+					</div>
+
+					<div class="remember">
+						{{ Form::checkbox('remember_user') . ' Remember Me' }} 
+					</div>
+
+					<div class="submit">
+						{{ Form::submit('Login!') }}
+					</div>
+
+				{{ Form::close() }}
+
+			</div> <!-- end login form -->
 
 		</div>
 	</body>
